43668a974d160c13dadeddd002e83c0231a6cad6
Quoc PHAN <phquoc25@gmail.com>
2019-06-02 16:19:48 +0700

    Adding some new avatars and OOP the JS

--- js/attendance-record.js
+++ js/attendance-record.js
@@ -3,7 +3,7 @@ class Attendee {
   constructor(name) {
     this.id = this.generateId();
     this.name = name;
-    this.avatar = "images/avatar2.png";
+    this.avatar = this.getRandomAvatar();
     this.presentOnTuesday = false;
     this.presentOnThursday = false;
   }
@@ -12,89 +12,85 @@ class Attendee {
     const date = new Date();
     return date.getTime();
   }
+
+  getRandomAvatar() {
+    const avatarNum = Math.floor(Math.random() * 5) + 1;
+    return "images/" + "avatar" + avatarNum + ".png";
+  }
 }
 
-class AttendanceRecord {
-  constructor() {
+class AttendanceRecordService {
+  constructor(attendanceRecordRender) {
+    this.attendanceRecordRender = attendanceRecordRender;
     this.attendees = [];
   }
 
   addAttendee(attendee) {
     this.attendees.push(attendee);
+    this.attendanceRecordRender.addAttendeeRow(attendee);
+  }
+
+  fetchAttendees() {
+    [new Attendee("Quoc PHAN"),
+    new Attendee("Pierre Roset"),
+    new Attendee("Gurval Le Bouter")].forEach(function (attendee){
+      this.addAttendee(attendee);
+    }.bind(this));
   }
 }
 
-function buildAttendeeRow(attendee) {
-  const rowContainer = document.createElement("div");
-  rowContainer.classList.add("row-container");
-  const namePanel = buildNamePanel(attendee.avatar, attendee.name);
-  const tuesdayCheck = buildCheckPanel();
-  const thursdayCheck = buildCheckPanel();
+class AttendanceRecordRender {
+  buildAttendeeRow(attendee) {
+    const rowContainer = document.createElement("div");
+    rowContainer.classList.add("row-container");
+    const namePanel = this.buildNamePanel(attendee.avatar, attendee.name);
+    const tuesdayCheck = this.buildCheckPanel();
+    const thursdayCheck = this.buildCheckPanel();
 
-  rowContainer.appendChild(namePanel);
-  rowContainer.appendChild(tuesdayCheck);
-  rowContainer.appendChild(thursdayCheck);
+    rowContainer.appendChild(namePanel);
+    rowContainer.appendChild(tuesdayCheck);
+    rowContainer.appendChild(thursdayCheck);
 
-  return rowContainer;
-}
+    return rowContainer;
+  }
 
-function buildCheckPanel() {
-  const checkOpt = document.createElement("div");
-  checkOpt.classList.add("check-opt");
-  const checkbox = document.createElement("input");
-  checkbox.type = "checkbox"
-  checkOpt.appendChild(checkbox);
-  return checkOpt;
-}
+  buildCheckPanel() {
+    const checkOpt = document.createElement("div");
+    checkOpt.classList.add("check-opt");
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox"
+    checkOpt.appendChild(checkbox);
+    return checkOpt;
+  }
 
-function buildNamePanel(avartarSrc, attendeeName) {
-  const namePanel = document.createElement("div");
-  namePanel.classList.add("name-panel");
+  buildNamePanel(avartarSrc, attendeeName) {
+    const namePanel = document.createElement("div");
+    namePanel.classList.add("name-panel");
 
-  const avatar = document.createElement("img");
-  avatar.classList.add("avatar");
-  avatar.src = avartarSrc;
+    const avatar = document.createElement("img");
+    avatar.classList.add("avatar");
+    avatar.src = avartarSrc;
 
-  const name = document.createElement("div");
-  name.classList.add("name");
-  name.innerHTML = attendeeName;
+    const name = document.createElement("div");
+    name.classList.add("name");
+    name.innerHTML = attendeeName;
 
-  namePanel.appendChild(avatar);
-  namePanel.appendChild(name);
+    namePanel.appendChild(avatar);
+    namePanel.appendChild(name);
 
-  return namePanel;
-}
+    return namePanel;
+  }
 
-function onAddNewClicked() {
-  const attendee = new Attendee('unname');
-  addAttendeeRow(attendee);
-  addAttendeeToCache(attendee);
-}
-
-function addAttendeeRow(attendee) {
-  const listBody = document.getElementById("list-body");
-  const attendeeRow = buildAttendeeRow(attendee);
-  listBody.appendChild(attendeeRow);
-}
-
-function addAttendeeToCache(attendee) {
-  let attendanceRecord = localStorage.getItem("attendanceRecord");
-  if (attendanceRecord) {
-    const attendees = JSON.parse(attendanceRecord).attendees;
-    attendanceRecord = new AttendanceRecord();
-    attendanceRecord.attendees = attendees;
-    attendanceRecord.addAttendee(attendee);
-    localStorage.setItem("attendanceRecord", JSON.stringify(attendanceRecord));
-  } else {
-    attendanceRecord = new AttendanceRecord();
-    attendanceRecord.addAttendee(attendee);
-    localStorage.setItem("attendanceRecord", JSON.stringify(attendanceRecord));
+  addAttendeeRow(attendee) {
+    const listBody = document.getElementById("list-body");
+    const attendeeRow = this.buildAttendeeRow(attendee);
+    listBody.appendChild(attendeeRow);
   }
 }
 
-let attendanceRecord = localStorage.getItem("attendanceRecord");
-if (attendanceRecord) {
-  JSON.parse(attendanceRecord).attendees.forEach(function(attendee) {
-    addAttendeeRow(attendee);
-  });
+// Global variables
+let attendanceRecordService;
+function initAttendanceRecordService() {
+  const attendanceRecordRender = new AttendanceRecordRender();
+  attendanceRecordService = new AttendanceRecordService(attendanceRecordRender);
 }
